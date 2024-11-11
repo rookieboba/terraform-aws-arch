1635a1f88ee2a76f91486d63f9b80aed6f59db1e
sungbin park <terranbin@gmail.com>
2024-11-11 16:20:20 +0900

    Update to free
    Update to free

--- variable.tf
+++ variable.tf
@@ -1,37 +1,32 @@
-variable "profile" {
-  description = "AWS User account Profile"
-}
-
 variable "region" {
-  default = "ap-south-1"
-}
-
-variable "key" {
-  description = "Enter Key name"
-}
-
-variable "sub_ids" {
-  default = []
-}
-
-variable "instance-ami" {
-  default = "ami-0b99c7725b9484f9e" # AMI of Mumbai region
+  description = "AWS 리전"
+  type        = string
+  default     = "ap-northeast-2"  # 서울 리전
 }
 
 variable "instance_type" {
-  default = "t3.medium"
+  description = "웹 및 애플리케이션 서버의 인스턴스 타입"
+  type        = string
+  default     = "t2.micro"  # 무료 티어 적용 가능
 }
 
-
-variable "cluster-name" {
-  description = "Cluster Name"
+variable "key_name" {
+  description = "EC2 인스턴스에 사용할 키 페어 이름"
+  type        = string
 }
 
-variable "server-name" {
-  description = "Ec2 Server Name"
+variable "db_instance_type" {
+  description = "RDS DB 인스턴스 타입"
+  type        = string
+  default     = "db.t2.micro"  # 무료 티어 적용 가능
 }
 
-variable "vpc_name" {
-  description = "VPC name"
+variable "vpc_id" {
+  description = "사용할 VPC ID"
+  type        = string
 }
-  
+
+variable "subnet_ids" {
+  description = "서브넷 ID 목록"
+  type        = list(string)
+}
