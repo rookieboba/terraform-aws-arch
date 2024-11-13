0de13fb0dab803a4834e5d87cf3419ba14510c61
sungbin park <terranbin@gmail.com>
2024-11-13 11:19:49 +0900

    든 변수를 포함하도록 스크립트를 수정하고, 사용자에게 입력을 받아 변수를 설정하도록 개선

--- create_tfvars.sh
+++ create_tfvars.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+read -p "Enter AWS region (default: ap-northeast-2): " region
+region=${region:-ap-northeast-2}
+
+read -p "Enter DB username (default: sungbin): " db_username
+db_username=${db_username:-sungbin}
+
+read -p "Enter DB password (default: sungbin): " db_password
+db_password=${db_password:-sungbin}
+
+read -p "Enter SSH public key path (default: ~/.ssh/id_rsa.pub): " ssh_public_key_path
+ssh_public_key_path=${ssh_public_key_path:-~/.ssh/id_rsa.pub}
+
 cat <<EOF > terraform.tfvars
-region = "ap-northeast-2"
-db_username = "sungbin"
-db_password = "sungbin"
+region = "$region"
+db_username = "$db_username"
+db_password = "$db_password"
+ssh_public_key_path = "$ssh_public_key_path"
 autoscaling_min_size = 1
 autoscaling_max_size = 4
-ssh_public_key_path = "~/.ssh/id_rsa.pub"
 EOF
