f4bd57b5d91142e69e6d9b52f663e6620a85d49b
sungbin park <terranbin@gmail.com>
2024-11-22 10:56:44 +0900

    Create terraform_install.sh
    added shell file for installing terraform

---
+++ terraform_install.sh
@@ -0,0 +1,5 @@
+yum install unzip -y
+curl -LO "https://releases.hashicorp.com/terraform/1.5.6/terraform_1.5.6_linux_amd64.zip"
+unzip terraform_1.5.6_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform -v
